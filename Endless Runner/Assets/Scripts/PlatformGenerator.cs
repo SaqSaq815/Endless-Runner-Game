846579360ec011aa0849d199fd039a314ea30454
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2022-07-11 22:09:48 -0400

    updated

--- Endless Runner/Assets/Scripts/PlatformGenerator.cs
+++ Endless Runner/Assets/Scripts/PlatformGenerator.cs
@@ -13,12 +13,23 @@ public class PlatformGenerator : MonoBehaviour
     public float distanceBetweenMin;
     public float distanceBetweenMax;
 
-    public ObjectPooler theObjectPool;
+    public GameObject[] thePlatforms;
+    private int platformSlector;
+    private float[] platformWidths;
+
+
+    //public ObjectPooler theObjectPool;
 
     // Start is called before the first frame update
     void Start()
     {
-        platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;    
+        //platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;    
+
+        platformWidths = new float[thePlatforms.Length];
+        for(int i = 0; i < thePlatforms.Length; i++)
+        {
+            platformWidths[i] = thePlatforms[i].GetComponent<BoxCollider2D>().size.x;
+        }
     }
 
     // Update is called once per frame
@@ -28,14 +39,17 @@ public class PlatformGenerator : MonoBehaviour
         {
             distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
 
-            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
-            //Instantiate(thePlatform, transform.position, transform.rotation);
+            platformSlector = Random.Range(0, thePlatforms.Length);
 
-            GameObject newPlatform = theObjectPool.GetPooledObject();
+            transform.position = new Vector3(transform.position.x + platformWidths[platformSlector] + distanceBetween, transform.position.y, transform.position.z);            
+
+            Instantiate(/*thePlatform*/ thePlatforms[platformSlector], transform.position, transform.rotation);
+
+           /* GameObject newPlatform = theObjectPool.GetPooledObject();
 
             newPlatform.transform.position = transform.position;
             newPlatform.transform.rotation = transform.rotation;
-            newPlatform.SetActive(true);
+            newPlatform.SetActive(true); */
         }
     }
 }

c73ab638ca109d8cb82142de4a705c04aa291b15
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2022-07-26 23:19:51 -0400

    Menu scene added

---
+++ Endless Runner/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public string PlayGameLevel;
+
+
+    public void PlayGame()
+    {
+        Application.LoadLevel(PlayGameLevel);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}

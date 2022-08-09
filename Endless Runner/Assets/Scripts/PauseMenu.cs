eb66d69b8417efcb4decd3f74e8d16cf25d2225e
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2022-08-08 23:15:27 -0400

    Pause Menu added

---
+++ Endless Runner/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public string mainMenuLevel;
+    public GameObject pauseMenu;
+
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(false);
+        FindObjectOfType<GameManager>().Reset();
+    }
+
+    public void QuitToMain()
+    {
+        Time.timeScale = 0f;
+        Application.LoadLevel(mainMenuLevel);
+    }
+}
